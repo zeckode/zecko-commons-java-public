950faf927b08a6515c361797726c3fbcf0c8cfeb
Jai Malani <71269258+jai38@users.noreply.github.com>
2022-11-30 23:07:05 +0530

    .gitignore is now working

--- target/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"co.zecko.common.java.pojo"}]
--- target/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"p":"co.zecko.common.java.pojo","l":"AbstractEntry"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"co.zecko.common.java.pojo","l":"BaseDataBaseEntry"},{"p":"co.zecko.common.java.pojo","l":"BaseEntry"},{"p":"co.zecko.common.java.pojo","l":"ErrorEntry"},{"p":"co.zecko.common.java.pojo","l":"Extensions"}]
